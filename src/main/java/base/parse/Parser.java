a34ada025408673e93c3378b1d726d586060b26e
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-01-16 17:31:05 +0200

    Added documentation to Parser interface

--- src/main/java/base/parse/Parser.java
+++ src/main/java/base/parse/Parser.java
@@ -3,9 +3,13 @@ package base.parse;
 import java.util.List;
 
 /**
- * The Parser interface represents objects that parse data of specific type from a file
+ * The Parser interface represents the ability to parse data of specific type from a file
  * @param <T> - The type of data to be parsed
  */
 public interface Parser<T> {
+    /**
+     * Get all the records from the file
+     * @return List of all records
+     */
     public List<T> getRecords();
 }
