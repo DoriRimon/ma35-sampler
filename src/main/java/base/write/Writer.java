7043d37c35e021904c41eb6827f1722c0d3e28d0
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-01-16 18:32:57 +0200

    Added IOExecption throws to methods in Writer interface

--- src/main/java/base/write/Writer.java
+++ src/main/java/base/write/Writer.java
@@ -1,5 +1,7 @@
 package base.write;
 
+import java.io.IOException;
+
 /**
  * The writer interface represents the ability to write objects of specific type to a file
  * @param <T> - The type of data to be written
@@ -8,11 +10,13 @@ public interface Writer<T> {
     /**
      * Add object to current batch
      * @param object - The object to be written
+     * @throws IOException - if writing failed
      */
-    public void write(T object);
+    public void write(T object) throws IOException;
 
     /**
      * Flush current batch into the destination
+     * @throws IOException - if writing of batch failed
      */
-    public void flush();
+    public void flush() throws IOException;
 }
