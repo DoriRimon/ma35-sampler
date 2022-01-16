4dfbd589e895b0e759a489cd9ebdd7d05922feb4
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-01-16 17:07:46 +0200

    Added getRecords method to Parser interface

--- src/main/java/base/parse/Parser.java
+++ src/main/java/base/parse/Parser.java
@@ -1,4 +1,11 @@
 package base.parse;
 
-public interface Parser {
+import java.util.List;
+
+/**
+ * The Parser interface represents objects that parse data of specific type from a file
+ * @param <T> - The type of data to be parsed
+ */
+public interface Parser<T> {
+    public List<T> getRecords();
 }
