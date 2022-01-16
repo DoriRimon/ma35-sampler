ebe5c74d02dcd2442fd191dd943c10b819614c28
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-01-16 17:33:14 +0200

    Created basic API of Writer interface

--- src/main/java/base/write/Writer.java
+++ src/main/java/base/write/Writer.java
@@ -1,4 +1,18 @@
 package base.write;
 
-public interface Writer {
+/**
+ * The writer interface represents the ability to write objects of specific type to a file
+ * @param <T> - The type of data to be written
+ */
+public interface Writer<T> {
+    /**
+     * Add object to current batch
+     * @param object - The object to be written
+     */
+    public void write(T object);
+
+    /**
+     * Flush current batch into the destination
+     */
+    public void flush();
 }
