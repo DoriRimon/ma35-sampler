1253fba3940f317e5125045fea99f2eae9b8a7fa
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-01-17 08:58:27 +0200

    Added streamData method call in Main class

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -1,7 +1,10 @@
 package run;
 
+import base.MadaReports;
+
 public class Main {
     public static void main(String[] args) {
-        
+        MadaReports madaReports = new MadaReports();
+        madaReports.streamData();
     }
 }
