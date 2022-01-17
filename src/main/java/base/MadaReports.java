a9c6ac712937b913b48b286522c48925257f94a7
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-01-17 08:57:01 +0200

    Created streamData method in MadaReports class

--- src/main/java/base/MadaReports.java
+++ src/main/java/base/MadaReports.java
@@ -3,11 +3,13 @@ package base;
 import base.parse.CSVParser;
 import base.parse.Parser;
 import base.types.madaReport.MadaReport;
+import base.types.madaReport.MadaReportUtils;
 import base.write.JsonWriter;
 import base.write.Writer;
 import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * The main class for Sampler - Stage A
@@ -29,6 +31,19 @@ public class MadaReports {
      * Streams data from the csv file to json files
      */
     public void streamData() {
-
+        List<CSVRecord> records = this.parser.getRecords();
+        records.remove(0); /* remove first row - columns names */
+        records.stream().map(MadaReportUtils::recordToMadaReport).forEach(report -> {
+            try {
+                this.writer.write(report);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        try {
+            this.writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
