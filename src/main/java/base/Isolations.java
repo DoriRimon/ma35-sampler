2eb054c7eb2521c7547a2378398a9049064323f0
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-01-18 10:20:15 +0200

    Created Isolations class

---
+++ src/main/java/base/Isolations.java
@@ -0,0 +1,68 @@
+package base;
+
+import base.types.isolated.Isolated;
+import base.types.isolated.IsolatedUtils;
+import base.types.labTest.LabTest;
+import base.types.madaReport.MadaReport;
+import base.write.JsonWriter;
+import base.write.WriteType;
+import base.write.Writer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * The main class for Sampler - Stage C
+ */
+public class Isolations {
+    private MadaReports madaReports;
+    private LabTests labTests;
+    private Writer<Isolated> writer;
+
+    public Isolations() {
+        this.madaReports = new MadaReports();
+        this.labTests = new LabTests();
+
+        try {
+            this.writer = new JsonWriter<>("jsonDirPath", WriteType.FILE_SIZE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Streams data from relevant CSV files to jsons
+     */
+    public void streamData() {
+        /* Create Map { id number -> mada report } */
+        HashMap<Integer, MadaReport> idToMadaReport = new HashMap<>();
+        List<MadaReport> madaReports = this.madaReports.getMadaReports();
+        madaReports.forEach(madaReport -> idToMadaReport.put(madaReport.getIdNum(), madaReport));
+
+        List<Isolated> isolates = new ArrayList<>();
+
+        /* Filter labTests to those that have corona, Join madaReports and labTests on idNum */
+        List<LabTest> labTests = this.labTests.getLabTests();
+        labTests.stream().filter(labTest -> labTest.getResultTestCorona() == 1).forEach(labTest -> {
+            MadaReport correspondingMadaReport = idToMadaReport.get(labTest.getIdNum());
+            Isolated isolated = IsolatedUtils.joinMadaReportAndLabTest(correspondingMadaReport, labTest);
+            isolates.add(isolated);
+        });
+
+        /* Write isolates to files */
+        isolates.forEach(isolated -> {
+            try {
+                this.writer.write(isolated);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        try {
+            this.writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
