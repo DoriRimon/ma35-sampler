b2bc1d6aeb6c02e1d5d8853332d6485c23cc7f47
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-01-18 10:20:46 +0200

    Changed main method in Main class to run Stage C

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -1,11 +1,12 @@
 package run;
 
+import base.Isolations;
 import base.LabTests;
 import base.MadaReports;
 
 public class Main {
     public static void main(String[] args) {
-        LabTests labTests = new LabTests();
-        labTests.streamData();
+        Isolations isolations = new Isolations();
+        isolations.streamData();
     }
 }
